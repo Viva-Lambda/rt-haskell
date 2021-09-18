63f162bbf4f26dc3d3c4333fa0e88babc2452253
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2021-09-18 14:21:09 +0200

    ray image kind of works

--- app/Main.hs
+++ app/Main.hs
@@ -5,21 +5,21 @@ import ColorIO
 import Color
 import GHC.Float
 import Vector
+import Ray
+import Prelude hiding(subtract)
 
+-- image related
 imageWidth :: Int
 imageHeight :: Int
 
-imageWidth = 256
-imageHeight = 256
+imageWidth = 400
+imageHeight = double2Int $ (int2Double imageWidth) / aspectRatio
 
-printPPMHeader :: IO ()
-printPPMHeader = do
-    putStrLn "P3"
-    putStrLn $ show imageWidth ++ " " ++ show imageHeight
-    putStrLn "255"
+aspectRatio :: Double
+aspectRatio = 16.0 / 9.0
 
 mkImageColCoordinate :: Int -> [Int] -> [(Int, Int)]
-mkImageColCoordinate col [] = []
+mkImageColCoordinate _ [] = []
 mkImageColCoordinate col (r:rows) =
     (col, r) : mkImageColCoordinate col rows
 
@@ -30,13 +30,69 @@ mkPixelCoordinates _ [] = []
 mkPixelCoordinates (c:cs) rows =
     mkImageColCoordinate c rows ++ mkPixelCoordinates cs rows
 
-mkPixelColor :: (Int,Int) -> Vector 
-mkPixelColor (xc,yc) =
-    let rval = (int2Double xc) / (int2Double imageWidth)
-        gval = (int2Double yc) / (int2Double imageHeight)
-        bval = 0.25
-        vec = VecFromList [rval, gval, bval]
-    in vec
+mkPixelRay :: (Int,Int) -> Ray 
+mkPixelRay (xc,yc) =
+    let u = (int2Double xc) / (int2Double (imageWidth -1))
+        v = (int2Double yc) / (int2Double (imageHeight - 1))
+        vvert = multiply (VecFromScalar v (vsize cameraV)) cameraV
+        uhor = multiply (VecFromScalar u (vsize cameraH)) cameraH
+        vvMinOr = subtract vvert cameraOrigin
+        uhorPlusVv = add uhor vvMinOr
+        llcPlusUHor = add lowerLeftCorner uhorPlusVv
+        ray = Rd {origin = cameraOrigin, direction = llcPlusUHor}
+    in ray
+
+
+rayColor :: Ray -> Vector
+rayColor Rd {origin = _, direction = b} =
+    let unitDirection = toUnit b
+        (VecFromList vdata) = fromScalarToList unitDirection
+        yval = vdata !! 1 -- access y value
+        tval = (yval + 1.0) * 0.5
+        cval = VecFromList [(1.0 - tval),(1.0 - tval),(1.0 - tval)]
+        oval = VecFromList [0.5, 0.7, 1.0]
+        other = multiply (VecFromScalar tval (vsize cval)) oval 
+    in add cval other
+
+mkPixelColor :: (Int, Int) -> Vector
+
+mkPixelColor a = rayColor $ mkPixelRay a
+
+-- camera related
+viewPortH :: Double
+viewPortH = 2.0
+viewPortW :: Double
+viewPortW = aspectRatio * viewPortH
+
+focalLength :: Double
+focalLength = 1.0
+
+cameraOrigin :: Vector
+cameraOrigin = VecFromList [0.0, 0.0, 0.0]
+cameraH :: Vector
+cameraH = VecFromList [viewPortW, 0.0, 0.0]
+
+cameraV :: Vector
+cameraV = VecFromList [ 0.0,viewPortH, 0.0]
+
+lowerLeftCorner :: Vector
+lowerLeftCorner = 
+    let fvec = VecFromList [0.0, 0.0, focalLength]
+        vhalf = divide cameraV (VecFromScalar 2.0 (vsize cameraV))
+        hhalf = divide cameraH (VecFromScalar 2.0 (vsize cameraH))
+        vMinF = subtract vhalf fvec
+        hMinV = subtract hhalf vMinF
+        origMinH = subtract cameraOrigin hMinV
+    in origMinH
+
+
+-- rendering ppm related
+printPPMHeader :: IO ()
+printPPMHeader = do
+    putStrLn "P3"
+    putStrLn $ show imageWidth ++ " " ++ show imageHeight
+    putStrLn "255"
+
 
 printCList :: Show a => [a] -> IO ()
 printCList lst = putStrLn $ unwords $ map show lst
--- src/Ray.hs
+++ src/Ray.hs
@@ -1,2 +1,15 @@
 -- ray module
 module Ray where
+
+import Vector
+
+data Ray = Rd {
+        origin :: Vector,
+        direction :: Vector
+    } deriving (Show, Eq)
+
+at :: Ray -> Double -> Vector
+at (Rd {origin = a, direction = b}) c =
+    let cVec = VecFromScalar c (vsize b)
+        cMultDir = multiply cVec b
+    in add a cMultDir
